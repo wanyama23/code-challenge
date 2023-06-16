318f5926923c9944c68d890302da5f4872bfd12a
edmond <edmond.wanyama@student.moringaschool.com>
2023-06-16 23:34:57 +0300

    Completed assignment

---
+++ student-Grade-Generator/index.js
@@ -0,0 +1,27 @@
+let marks = 70
+
+if (marks >=79) {
+    console.log("A");
+    
+}
+
+else if (marks >60 && marks <79) {
+    console.log("B");
+    
+}
+
+else if (marks >=49 && marks<=59) {
+    console.log("C");
+    
+}
+
+else if (marks >49 && marks<=40) {
+    console.log("D");
+    
+}
+
+else if (marks <40) {
+    console.log("E");
+    
+}
+
