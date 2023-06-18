3ed1e93d371b4d2f9da5624fd6ee9de2fdb1404a
wanyama23 <135007330+wanyama23@users.noreply.github.com>
2023-06-18 14:50:58 +0300

    Add files via upload

--- Speed-Detector/index.js
+++ Speed-Detector/index.js
@@ -1,15 +1,27 @@
-detectSpeed(70);
+const prompt = require('prompt-sync')();
 
-function detectSpeed(speed) {
+let  userSpeed = prompt("Enter Your Speed");
+
+
+checkSpeed(Number(userSpeed));
+
+
+function checkSpeed(speed) {
   const speedLimit = 70;
   const kmPerPoint = 5;
 
-  if (speed < speedLimit + kmPerPoint) {
+  if (speed <= speedLimit) {
     console.log("Ok");
     return;
+  }else {
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12) {console.log("License suspended");}
+    else {console.log("Points", points);
+}
+
   }
 
-  const points = Math.floor((speed - speedLimit) / kmPerPoint);
-  if (points >= 12) console.log("License suspended");
-  else console.log("Points", points);
-}
+
+  }
+
+  
